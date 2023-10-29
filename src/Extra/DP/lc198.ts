af6f80c5d1241f2a92e72a3a39589d3a235dfa96
jacgit18 <joshuaxcarpentier@gmail.com>
2023-10-29 10:12:29 -0400

    edit

--- src/Extra/DP/lc198.ts
+++ src/Extra/DP/lc198.ts
@@ -7,6 +7,9 @@
    ------------------------
    Vairiable:
    
+
+
+   Encapusalation/closure
 */
 const myrob = () => {
     let cache={};
@@ -30,10 +33,23 @@ const myrob = () => {
 
 
 let price = [1,2,3,1];
+// acts a private method or vairiabe
 
-const myrobObj = myrob();
+const myrobObj = myrob(); 
 
-console.log(myrobObj(price,price.length -1))
+// if this function took a param it would look like this 
+
+// Closure is used here
+// const newFunction = myrob('out'); first param
+// console.log(newFunction);
+// console.log(newFunction(price,price.length -1)); return function second param
+
+
+
+console.log(myrobObj(price,price.length -1)) // using currying here 
+// newFunction('out')(price,price.length -1) // altertively you can curry like this
+
+
 // console.log(rob(price))
 
 console.log(`\n ------- Edge Case -------- `);
