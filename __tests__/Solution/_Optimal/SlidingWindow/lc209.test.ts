7151dbf21fc10f7544b9423c59774cd97f0df9b4
jacgit18 <joshuaxcarpentier@gmail.com>
2023-11-08 23:00:33 -0500

    updated simpler test structure

--- __tests__/Solution/_Optimal/SlidingWindow/lc209.test.ts
+++ __tests__/Solution/_Optimal/SlidingWindow/lc209.test.ts
@@ -1,97 +1,66 @@
-// import { toBe } from '@jest/globals';
-import * as Benchmark from 'benchmark';
 import { combinedAttemptExports } from "../../../../src/routers/OptimalAttempt";
 import { combinedSolutionExports } from "../../../../src/routers/OptimalSolution";
 
+type TestFunction = (s: number, arr: number[]) => number;
 
-describe('smallest_subarray_with_given_sum', () => {
-  let lc209: (s: number, arr: number[]) => number;
-  let lc209Alt: (s: number, arr: number[]) => number;
+const generateTestCases = (
+  lcFunction: TestFunction,
+  input: number,
+  arr: number[],
+  testName: string,
+  expected: number
+) => {
+  describe(`smallest_subarray_with_given_sum ${testName}`, () => {
+    let lcFn: TestFunction;
 
-  beforeEach(() => {
-    lc209 = combinedSolutionExports.OptimalSlidingWindSolution.lc209;
-    lc209Alt = combinedSolutionExports.OptimalSlidingWindSolution.lc209alt;
+    beforeEach(() => {
+      lcFn = lcFunction;
+    });
+
+    it(`should output ${expected} when input is ${input} and arr is [${arr}]`, () => {
+      const result = lcFn(input, arr);
+      console.time(`lc${testName}`);
+      expect(result).toBe(expected);
+      console.timeEnd(`lc${testName}`);
+    });
   });
+};
 
-  it('should compare the speed of lc209 and lc209Alt', (done) => {
-    // Define a benchmark suite
-    const suite = new Benchmark.Suite();
-  // Dont need to bench mark all the time only doing it for files with multiple approaches
-    suite
-      .add('lc209', () => {
-        lc209(7, [2, 1, 5, 2, 3, 1]);
-      })
-      .add('lc209Alt', () => {
-        lc209Alt(7, [2, 1, 5, 2, 3, 1]);
-      })
-      // .add('lc209_LargeArray', () => {
-      //   lc209(10, Array.from({ length: 5 }, () => Math.floor(Math.random())));
-      // })
-      // .add('lc209Alt_LargeArray', () => {
-      //   lc209Alt(10, Array.from({ length: 5 }, () => Math.floor(Math.random() * 10)));
-      // })
-      .on('cycle', (event: Benchmark.Event) => {
-        console.log(String(event.target)); // Print benchmark results
-      })
-      .on('complete', () => {
-        console.log('Benchmark completed.'); // Benchmark suite completed
-        done(); // Signal that the test is done
-      })
-      .run({ async: true }); // Run the benchmark asynchronously
+const runTestCases = () => {
+  const testCases = [
+    { input: 7, arr: [2, 1, 5, 2, 3, 1], expect: 2 },
+    
+    // Add more test cases here
+  ];
 
-    // You can add assertions here if needed
-  }, 30000); // Increase the timeout to 10 seconds (adjust as needed)
-});
+  testCases.forEach((testCase, index) => {
+    // Test the Attempt approach
+    // generateTestCases(
+    //   combinedAttemptExports.OptimalSlidingWindAttempt.lc209,
+    //   testCase.input,
+    //   testCase.arr,
+    //   `209 Attempt - Test Case ${index + 1}`,
+    //   testCase.expect
+    // );
 
+    // Test the Solution approach
+    generateTestCases(
+      combinedSolutionExports.OptimalSlidingWindSolution.lc209,
+      testCase.input,
+      testCase.arr,
+      `209 Solution - Test Case ${index + 1}`,
+      testCase.expect
+    );
 
-
-describe('smallest_subarray_with_given_sum Attempt', () => {
-  let lc209Attempt: (s: number, arr: number[]) => number;
-  let lc209: (s: number, arr: number[]) => number;
-  let lc209Alt: (s: number, arr: number[]) => number;
-
-
-  beforeEach(() => {
-    lc209Attempt = combinedAttemptExports.OptimalSlidingWindAttempt.lc209;
-    lc209 = combinedSolutionExports.OptimalSlidingWindSolution.lc209;
-    lc209Alt = combinedSolutionExports.OptimalSlidingWindSolution.lc209alt;
-
-    // Update cases
-    const testCases = [
-      { input: "ABAB", k: 2, expected: 4 },
-      { input: "AABABBA", k: 1, expected: 4 },
-      { input: "ABCDE", k: 1, expected: 2 },
-      { input: "AAAA", k: 2, expected: 4 },
-      { input: "AAAB", k: 0, expected: 3 },
-      { input: "AABA", k: 0, expected: 2 },
-    ];
-
+    // Test the Alt Approach
+    generateTestCases(
+      combinedSolutionExports.OptimalSlidingWindSolution.lc209alt,
+      testCase.input,
+      testCase.arr,
+      `209 Alt Solution - Test Case ${index + 1}`,
+      testCase.expect
+    );
   });
+};
 
-  it('should output first param passed for empty attempt', () => {
-
-
-    const result1 = lc209Attempt(7, [2, 1, 5, 2, 3, 1]);
-    console.time('lc209 Attempt');
-    expect(result1).toBe(7);
-    console.timeEnd('lc209 Attempt');
-
-  });
-
-  it('Init Approach should output 2', () => {
-    const result12 = lc209(7, [2, 1, 5, 2, 3, 1]);
-    console.time('lc209');
-    expect(result12).toBe(2);
-    console.timeEnd('lc209');
-
-  });
-
-  it('Alt Approach should output 2', () => {
-    const result123 = lc209Alt(7, [2, 1, 5, 2, 3, 1]);
-    console.time('lc209Alt');
-    expect(result123).toBe(2); 
-    console.timeEnd('lc209Alt');
-
-  });
-
-});
+runTestCases();
