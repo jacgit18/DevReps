48b8b217367f013fb04005cb0c277e6f34dad9c6
jacgit18 <joshuaxcarpentier@gmail.com>
2023-11-10 16:29:52 -0500

    edit

--- src/util/_DataStructuresBuiltUpClasses to review/Trees/BinaryTreeIterativeWithTraverse.ts
+++ src/util/_DataStructuresBuiltUpClasses to review/Trees/BinaryTreeIterativeWithTraverse.ts
@@ -236,7 +236,7 @@ console.log(tree)
 //  4     20
 //1  6  15  170
 
-function traverse(node) {
+function traverser(node) {
   const tree = { value: node.value }
   tree.left = node.left === null ? null : traverse(node.left)
   tree.right = node.right === null ? null : traverse(node.right)
