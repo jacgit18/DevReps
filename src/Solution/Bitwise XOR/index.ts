e4fecdeecf6d1370f14bbfd3c0cca3fd851785e2
jacgit18 <joshuaxcarpentier@gmail.com>
2023-12-06 09:35:35 -0500

    added more challeges

--- src/Solution/Bitwise XOR/index.ts
+++ src/Solution/Bitwise XOR/index.ts
@@ -1,6 +1,13 @@
 import { singleNumber } from "../Bitwise XOR/lc136"
+import { singleNumberThree } from "./lc260"
+import { findComplemen } from "./lc476"
+import { flipAndInvertImage } from "./lc832"
 
 
 export const SolutionBitWiseXOR = {
   lc136: singleNumber,
+  lc260: singleNumberThree,
+  lc476: findComplemen,
+  lc832: flipAndInvertImage,
+
 }
