2592bedd35befecf2a3bda964defafa65ef9af04
jacgit18 <joshuaxcarpentier@gmail.com>
2023-12-06 16:53:51 -0500

    added challenges

---
+++ src/Solution/Heaps/index.ts
@@ -0,0 +1,12 @@
+import { MedianFinder } from "./lc295"
+import { findRightInterval } from "./lc436"
+import { medianSlidingWindow } from "./lc480"
+import { findMaximizedCapital } from "./lc502"
+
+
+export const SolutionHeaps = {
+  lc295: MedianFinder,
+  lc436: findRightInterval,
+  lc480: medianSlidingWindow,
+  lc502: findMaximizedCapital,
+}
