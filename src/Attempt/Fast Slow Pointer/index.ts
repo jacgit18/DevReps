fd008ccbec43fa068f299da02e3defb61ae1df4c
jacgit18 <joshuaxcarpentier@gmail.com>
2023-12-06 08:37:56 -0500

    add more problems and test placeholder

--- src/Attempt/Fast Slow Pointer/index.ts
+++ src/Attempt/Fast Slow Pointer/index.ts
@@ -1,10 +1,8 @@
 import { hasCycle } from "./lc141CyclicLL";
-import { isDoublyCyclic } from "./lc141DoubleVer";
 import { isHappy } from "./lc202";
 
 
 export const AttemptFastSlowPointers  = {
   lc141: hasCycle,
-  lc141Double: isDoublyCyclic,
   lc202: isHappy,
 };
--- src/Solution/Fast Slow Pointer/index.ts
+++ src/Solution/Fast Slow Pointer/index.ts
@@ -1,9 +1,17 @@
-import { hasCycle } from "../Fast Slow(LinkedList)/lc141CyclicLL"
-import { isDoublyCyclic } from "../Fast Slow(LinkedList)/lc141DoubleVer"
-import { isHappy } from "../Fast Slow(LinkedList)/lc202"
+import { hasCycle } from "./lc141CyclicLL"
+import { detectCycle } from "./lc142"
+import { reorderList } from "./lc143"
+import { isHappy } from "./lc202"
+import { isPalindrome } from "./lc234"
+import { circularArrayLoop } from "./lc457"
+import { middleNode } from "./lc876"
 
 export const SolutionFastSlowPointer = {
   lc141: hasCycle,
-  lc141Double: isDoublyCyclic,
+  lc142: detectCycle,
+  lc143: reorderList,
   lc202: isHappy,
+  lc234: isPalindrome,
+  lc457: circularArrayLoop,
+  lc876: middleNode
 }
