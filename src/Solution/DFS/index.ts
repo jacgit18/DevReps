5964b3c362c7800eb4431c35af661e291191f304
jacgit18 <joshuaxcarpentier@gmail.com>
2023-12-06 09:42:27 -0500

    added challenges

--- src/Solution/DFS/index.ts
+++ src/Solution/DFS/index.ts
@@ -5,13 +5,18 @@ import { sumNumbers } from "../DFS/lc129"
 import { isValidSequence } from "../DFS/lc1430"
 import { inorderSuccessor } from "../DFS/lc285"
 import { pathSum } from "../DFS/lc437"
+import { maxPathSum } from "./lc124"
+import { diameterOfBinaryTree } from "./lc543"
+
 
 export const SolutionDFS = {
   lc112: hasPathSum,
   lc113: pathSumTwo,
+  lc124: maxPathSum,
   lc129: sumNumbers,
   lc257: binaryTreePaths,
   lc285: inorderSuccessor,
   lc437: pathSum,
+  lc543: diameterOfBinaryTree,
   lc1430: isValidSequence,
 }
