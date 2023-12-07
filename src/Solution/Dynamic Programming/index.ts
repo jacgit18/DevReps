4cb130f9918febdc316c3d5d2beaae371c436cbf
jacgit18 <joshuaxcarpentier@gmail.com>
2023-12-07 03:39:00 -0500

    more test place holders and mv

--- src/Solution/Dynamic Programming/index.ts
+++ src/Solution/Dynamic Programming/index.ts
@@ -1,7 +1,7 @@
 import { rob } from "../Dynamic Programming/lc198"
 import { coinChange } from "../Dynamic Programming/lc322"
 import { longestPalindrome } from "../Dynamic Programming/lc5"
-import { minStepsToOne } from "./minStepsToOne"
+import { minStepsToOne } from "../../CodeSnippetsToReview/DP/minStepsToOne"
 
 export const SolutionDynamicPro = {
   lc322: coinChange,
