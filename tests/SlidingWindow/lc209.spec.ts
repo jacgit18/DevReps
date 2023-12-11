8c1acc27ed5a24f38b0619fe4915ae835d4a9c9c
jacgit18 <joshuaxcarpentier@gmail.com>
2023-12-11 04:17:34 -0500

    clean up

--- tests/SlidingWindow/lc209.spec.ts
+++ tests/SlidingWindow/lc209.spec.ts
@@ -26,23 +26,23 @@ const runTestCases = () => {
       { params: [0, [1, 2, 3]], expected: 0 }, // Window size zero
       ];
 
-      // const testCasesWeridBehavior: TestCase[] = [
-      //   { params: 7, paramsTwo: [2, 3, 1, 2, 4, 3], expected: 2 },
-      //   { params: 4, paramsTwo:[1, 4, 4], expected: 1 },
-      //   { params: 11, paramsTwo: [1, 1, 1, 1, 1, 1, 1, 1], expected: 0 },
-      //   { params: 3, paramsTwo: [1, 1, 1, 1, 1], expected: 3 }, // Window size equal to array length
-      //   { params: 5, paramsTwo: [1, 2, 3, 4, 5], expected: 1 }, // Window size larger than array length
-      //   { params: 0, paramsTwo: [0, []], expected: 0 }, // Empty array
-      //   { params: 1, paramsTwo: [1, [5]], expected: 1 }, // Window size equal to array length with a single element
-      //   { params: 0, paramsTwo: [0, [1, 2, 3]], expected: 0 } // Window size zero
-      //   ];
+      const testCasesWeridBehavior: TestCase[] = [
+        { params: 7, paramsTwo: [2, 3, 1, 2, 4, 3], expected: 2 },
+        { params: 4, paramsTwo:[1, 4, 4], expected: 1 },
+        { params: 11, paramsTwo: [1, 1, 1, 1, 1, 1, 1, 1], expected: 0 },
+        { params: 3, paramsTwo: [1, 1, 1, 1, 1], expected: 3 }, // Window size equal to array length
+        { params: 5, paramsTwo: [1, 2, 3, 4, 5], expected: 1 }, // Window size larger than array length
+        { params: 0, paramsTwo: [0, []], expected: 0 }, // Empty array
+        { params: 1, paramsTwo: [1, [5]], expected: 1 }, // Window size equal to array length with a single element
+        { params: 0, paramsTwo: [0, [1, 2, 3]], expected: 0 } // Window size zero
+        ];
       
   
       generateTestCases(AttemptSlidingWindow.lc209.smallestSubarrayWithGivenSumBruteForce, testCasePlaceholder, ' BruteForce Code Attempt ');
       generateTestCases(AttemptSlidingWindow.lc209.smallestSubarrayWithGivenSumNaiveDec, testCasePlaceholder,' Naive Declaritive Code Attempt ');
 
-      generateTestCases(AttemptSlidingWindow.lc209.smallestSubarrayWithGivenSumNaiveImp, testCasePerformance,  ' Naive Imperative Code Attempt ');
-      generateTestCases(AttemptSlidingWindow.lc209.smallestSubarrayWithGivenSumOptimal, testCasePerformance, ' Optimal Code Attempt ');
+      // generateTestCases(AttemptSlidingWindow.lc209.smallestSubarrayWithGivenSumNaiveImp, testCasePerformance,  ' Naive Imperative Code Attempt ');
+      // generateTestCases(AttemptSlidingWindow.lc209.smallestSubarrayWithGivenSumOptimal, testCasePerformance, ' Optimal Code Attempt ');
 
       generateTestCases(SolutionSlidingWindow.lc209.smallestSubarraySlidingWindow, testCases, ' Solution ');
       generateTestCases(SolutionSlidingWindow.lc209.smallest_subarray_with_given_sumAlt, testCases, 'Alt Solution ');
--- tests/TwoPointers/lc1.spec.ts
+++ tests/TwoPointers/lc1.spec.ts
@@ -31,8 +31,6 @@ const runTestCases = () => {
       { params: [2, 7, 11, 15], paramsTwo: 9, expected: [0, 1]},
       { params: [3, 2, 4], paramsTwo: 6, expected: [1, 2]},
       { params: [3, 3], paramsTwo: 6, expected: [0, 1]},
-
-      // Add more test cases here
     ];
 
 
@@ -43,11 +41,11 @@ const runTestCases = () => {
       // generateTestCases(AttemptTwoPointer.lc1.twoSumNaiveImp, testCasePerformance, ' Naive Imperative Code Attempt ');
       // generateTestCases(AttemptTwoPointer.lc1.twoSumOptimal, testCasePerformance, ' Optimal Code Attempt ');
 
-      generateTestCases(SolutionTwoPointer.lc1.twoSumTwoPointer, testCases, ' Solution ');
-      generateTestCases(SolutionTwoPointer.lc1.twoSumAlt, testCases, ' Alt Solution ');
+      // generateTestCases(SolutionTwoPointer.lc1.twoSumTwoPointer, testCasePerformance, ' Solution ');
+      // generateTestCases(SolutionTwoPointer.lc1.twoSumAlt, testCasePerformance, ' Alt Solution ');
 
-  
-    
+      generateTestCases(SolutionTwoPointer.lc1.twoSumTwoPointer, testCases, ' Solution ');
+
     };
     
     runTestCases();
